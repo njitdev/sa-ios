3c76d7f74a10ae528f900873e8c2eedf825d5413
Yunzhu Li <contact@yunzhu.li>
2017-03-11 03:23:44 -0500

    Basic app skeleton with CocoaPods (#1)

---
+++ sa-ios/sa-ios/src/home/HomeViewController.swift
@@ -0,0 +1,17 @@
+//
+//  HomeViewController.swift
+//  sa-ios
+//
+//  Created by Yunzhu Li on 3/10/17.
+//
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+}
---
+++ sa-ios/sa-ios/src/msgboard/MessageBoardViewController.swift
@@ -0,0 +1,18 @@
+//
+//  MessageBoardViewController.swift
+//  sa-ios
+//
+//  Created by Yunzhu Li on 3/10/17.
+//
+//
+
+import UIKit
+
+class MessageBoardViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
